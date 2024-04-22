9a66927ad0ec8a96aa7a3c52b60a45b25c3352c8
Asim Khan <asimseom@gmail.com>
2024-04-22 10:36:23 +0500

    Operator

---
+++ 4.Operators.py
@@ -0,0 +1,17 @@
+#Arithmetic Operators
+
+print ("sum", 4+ 3)
+print ("difference", 4 - 3)
+print ("multiply", 4 * 3)
+print ("Division", 4 / 3)
+
+print ("floor division", 4 // 3)
+print ("remainder", 4 % 3)
+print ("exponential", 4 ** 3)
+
+#Comparison Operators
+
+mid_marks =22 
+final_marks = 33
+
+print (final_marks > mid_marks)
