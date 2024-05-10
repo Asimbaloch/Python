30b2e2b9fd7225bc2a52de95b9225075f33ec250
Asim Khan <asimseom@gmail.com>
2024-05-10 22:07:59 +0500

    Worked on the strings

--- 6.Strings.py
+++ 6.Strings.py
@@ -2,3 +2,30 @@
 statement =" Hello everyone! I am writing this string to print the string here.\n It is the new line."
 
 print (statement)
+
+#Concatenation
+first_name ="Asim"
+print(first_name.replace("Asim","Ahsan"))
+last_name ="Khan"
+
+complete_name= first_name +" " +last_name
+print(complete_name.capitalize())
+
+
+#Length of String
+print(len(complete_name))
+
+print (len(first_name))
+
+
+
+#Indexing
+university_name = "Lahore School of Computer Sciences"
+print (university_name[3])
+
+#Slicing
+print(university_name[3:6])
+print(university_name[1:4])
+print(university_name[7:])
+print(university_name[-5:-1]) 
+
