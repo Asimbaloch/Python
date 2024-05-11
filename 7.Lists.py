9964daa06c0b69cd6c3a4dea513af36ff672aea2
Asim Khan <asimseom@gmail.com>
2024-05-11 11:19:17 +0500

    List

---
+++ 7.Lists.py
@@ -0,0 +1,12 @@
+marks =[22.3,44.4,58.9]
+print(marks)
+print(type(marks))
+
+#Mutable and can store any data type
+
+student_record = ["Ali",22, "lahore"]
+print(student_record)
+
+print(student_record[2])
+student_record[1] =33
+print(student_record)
