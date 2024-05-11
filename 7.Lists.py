32292e925ea428bf7be00749caa8deb598e64269
Asim Khan <asimseom@gmail.com>
2024-05-11 11:50:55 +0500

    list practice completed

--- 7.Lists.py
+++ 7.Lists.py
@@ -1,5 +1,8 @@
 marks =[22.3,44.4,58.9]
+marks.insert(1,33)
+marks.sort()
 print(marks)
+
 print(type(marks))
 
 #Mutable and can store any data type
@@ -10,3 +13,34 @@ print(student_record)
 print(student_record[2])
 student_record[1] =33
 print(student_record)
+
+print(student_record[0:2])
+print(student_record[-3:-1])
+
+#List Methods
+#Append (Adds one element at the end)
+list = [1,2,3]
+list.append(4)
+list.append(8)
+list.append(7)
+
+#Sort (sorts in ascending order)
+list.sort()
+
+
+print(list)
+
+#sort (sort in decending order)
+list.sort(reverse=True)
+
+print(list)
+
+#Reverse
+list.reverse()
+print(list)
+
+
+cs_class =["Asim",3.29,"Python"]
+cs_class.remove(3.29)
+cs_class.pop(0)
+print(cs_class)
