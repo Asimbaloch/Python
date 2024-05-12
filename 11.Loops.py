f078db714a08f9d1f15c73f213ca17bf1e168093
Asim Khan <asimseom@gmail.com>
2024-05-12 18:57:41 +0500

    working on loops

---
+++ 11.Loops.py
@@ -0,0 +1,28 @@
+#While loops
+count=1
+while count<=5:
+    print("Hello everyone!")
+    count+=1
+
+    i=1
+    while i <= 5:
+        print("check")
+        i+=1
+
+
+
+
+#Q - Print numbers from 1 to 100.
+i = 1
+while i<=100:
+    print(i)
+    i+=1
+
+#Q - Print numbers from 100 to 1.
+
+num1 = 100
+while num1>=1:
+    print(num1)
+    num1 -=1
+
+
