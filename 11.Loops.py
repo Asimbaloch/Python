769e5b388f4d8fe6dfac848402c5b621c4ba6e89
Asim Khan <asimseom@gmail.com>
2024-05-14 11:29:03 +0500

    Printing elements through loops

--- 11.Loops.py
+++ 11.Loops.py
@@ -31,3 +31,13 @@ t = 1
 while t<=10:
     print(n*t)
     t+=1
+
+#WAP to print the elements of the loop.
+nums =[1,4,9,16,25,36,49,64,81,100]
+
+idx= 0
+
+while idx < len(nums):
+    print(nums[idx])
+    idx+=1
+
