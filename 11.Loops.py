dbacf8ee9da1e6d1d7f2528a313002fc1796f82c
Asim Khan <asimseom@gmail.com>
2024-05-15 13:19:38 +0500

    loops

--- 11.Loops.py
+++ 11.Loops.py
@@ -41,3 +41,11 @@ while idx < len(nums):
     print(nums[idx])
     idx+=1
 
+#Search for a number x in this tuple using loop"
+roll_numbers = (1,4,9,16,25,36,49,64,81,100)
+x=16
+i = 0
+while i<len(roll_numbers):
+    if (nums[i]==x):
+        print("Found at", i)
+    i+=1
