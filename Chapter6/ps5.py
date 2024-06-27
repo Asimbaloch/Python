d8a6651f713ea8564f13811227378931c12da262
Asim Khan <asimseom@gmail.com>
2024-06-27 11:02:18 +0500

    Exercise of conditionals done

---
+++ Chapter6/ps5.py
@@ -0,0 +1,9 @@
+l = ["Asim","Ahsan","Mahad"]
+
+name = input ("Enter the name: ")
+
+if (name in l):
+    print("The name is in the list.")
+
+else:
+    print("Your  name is not in the list")
---
+++ Chapter6/ps7.py
@@ -0,0 +1,7 @@
+post = "I am checking the result right now"
+
+if ("result" in post):
+    print("It is talking about the result")
+
+else:
+    print("This post is not talking about the result.")
