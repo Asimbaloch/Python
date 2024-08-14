829a40ba76940b7c1613748d860b5f864f2e0e2a
Asim Khan <asimseom@gmail.com>
2024-08-14 12:44:53 +0500

    Variables and operators code

---
+++ 02_variables.py
@@ -0,0 +1,27 @@
+#Variables rules
+#1. Variables names must start with letters or underscore
+#2. Variables names must not start with numbers
+#3. Variables names are case sensitive
+#4. Variables names can contain only letters, numbers and underscores
+#5. Variables can store different data types
+my_name = "Asim"
+print(my_name)
+my_age = 25
+print(my_age)
+my_height = 1.75
+print(my_height)
+print(type(my_name))
+print(type(my_age))
+print(type(my_height))
+
+my_bool = True
+my_none = None
+my_tuple = ('a', 'b', 'c')
+my_list = [1, 2, 3]
+my_dict = {'name': 'Asim', 'age': 25}
+print(type(my_bool))
+print(type(my_none))
+print(type(my_tuple))
+print(type(my_list))
+print(type(my_dict))
+print("-----------------------------------------------------------------")
